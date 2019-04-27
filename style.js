dbc031a18eb3f105ebfefc6a75df1794cec5f663
johnwanjema <jonwanjema@gmail.com>
2019-04-27 10:28:56 +0300

    creates auser input for the lengths

--- style.js
+++ style.js
@@ -2,6 +2,9 @@
 
 function triangle(length1, length2, length3){
     var lengths=[length1, length2, length3]
-    Length1 = prompt("enternumber")
+    Length1 = prompt("enter length")
+    Length2 = prompt("enter length")
+    Length3 = prompt("enter length")
+
 }
 
