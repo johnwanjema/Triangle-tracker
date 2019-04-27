f8b4e905ff41664e317f817e55b1bf80f7fd6d61
johnwanjema <jonwanjema@gmail.com>
2019-04-27 10:54:22 +0300

    this commit adds if function to determine type of triangle

--- style.js
+++ style.js
@@ -2,9 +2,18 @@
 
 function triangle(length1, length2, length3){
     var lengths=[length1, length2, length3]
-    Length1 = prompt("enter length")
-    Length2 = prompt("enter length")
-    Length3 = prompt("enter length")
+    length1 = prompt("enter length1")
+    length2 = prompt("enter length2")
+    length3 = prompt("enter length3")
+
+    if (length1===length2 & length2===length3){
+        alert("equilateral triangle")
+    } else if (length1===length2 & length2!==length3 || length1!==length2 & length2==length3){
+        alert("isoselice triangle")
+    } else if (length1!==length2 & length2!==length3){
+        alert("scalene triangle")
+    }
+        
 
 }
 
