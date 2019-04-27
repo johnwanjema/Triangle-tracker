a997394465e0a91d506f1d477b81de2fe7865eda
johnwanjema <jonwanjema@gmail.com>
2019-04-27 12:29:52 +0300

    checks that the user does not input zeros as length

--- style.js
+++ style.js
@@ -5,12 +5,21 @@ function triangle(length1, length2, length3){
     length1 = prompt("enter length1")
     length2 = prompt("enter length2")
     length3 = prompt("enter length3")
+    
 
-    if (length1===length2 & length2===length3){
+    var sum  =length1 + length2;
+
+    
+
+    if (length1===length2 & length2===length3 & length1>0 & length2>0 & length3>0){
         alert("equilateral triangle")
-    } else if (length1===length2 & length2!==length3 || length1!==length2 & length2==length3){
+    }
+    
+    else if (length1===length2 & length2!==length3 || length1!==length2 & length2==length3 & 
+             length1>0 & length2>0 & length3>0){
         alert("isoselice triangle")
-    } else if (length1!==length2 & length2!==length3){
+        
+    } else if ( sum > length3){
         alert("scalene triangle")
     }
         
