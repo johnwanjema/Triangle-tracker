d1da24a8f32665c0c00d05f85e3c2bf516eade76
johnwanjema <jonwanjema@gmail.com>
2019-04-28 07:42:51 +0300

    alerts the user not to input zeros

--- style.js
+++ style.js
@@ -2,25 +2,31 @@
 
 function triangle(length1, length2, length3){
     var lengths=[length1, length2, length3]
-    length1 = prompt("enter length1")
-    length2 = prompt("enter length2")
-    length3 = prompt("enter length3")
+    length1 = parseInt(prompt("enter length1"))   
+    length2 = parseInt(prompt("enter length2")) 
+    length3 = parseInt(prompt("enter length3")) 
+    var totalLength = length1 + length2;
 
-    
+    alert(totalLength)
 
-    if (length1===0 && length2===0 && length3===0){
-        alert(" wghjjhgjhjkjsdfghjkdfghjfghj")
+
+    if(totalLength==5){
+        alert("ertyuiorhjkertyuidfghjrghjk")
+    }
+
+
+    if (  length1==0 || length2==0 || length3==0 ){
+        alert(" input  numbers greater than 0")
     }else if (length1===length2 && length2===length3 ){
         alert("equilateral triangle")
     } else if (length1===length2 & length2!==length3 || length1!==length2 & length2==length3){
         alert("isoselice triangle")
     } else if (length1!==length2 & length2!==length3 &length1!==length3){
         alert("scalene triangle")
-    } else if (length1===0 || length2===0 ||length3===0){
-        alert(" wghjjhgjhjkjsdfghjkdfghjfghj")
+    } else if( totalLength==0 ){
+        alert("wabebebe mimi")
+
     }
-    
-        
 
 }
 
