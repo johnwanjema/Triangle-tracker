57eeb45af884ede4bb1d945c33cb9226c266c2d5
johnwanjema <jonwanjema@gmail.com>
2019-04-28 18:47:45 +0300

    this commit makes sure the sum of any two side is more than the remaining side

--- style.js
+++ style.js
@@ -5,27 +5,17 @@ function triangle(length1, length2, length3){
     length1 = parseInt(prompt("enter length1"))   
     length2 = parseInt(prompt("enter length2")) 
     length3 = parseInt(prompt("enter length3")) 
-    var totalLength = length1 + length2;
-
-    alert(totalLength)
+      
 
 
-    if(totalLength==5){
-        alert("ertyuiorhjkertyuidfghjrghjk")
-    }
-
-
-    if (  length1==0 || length2==0 || length3==0 ){
-        alert(" input  numbers greater than 0")
+    if (  length1 + length2 <= length3 || length1 + length3 <= length2|| length2 + length3 <= length1){
+        alert(" The values you entered cant form a triangle")
     }else if (length1===length2 && length2===length3 ){
         alert("equilateral triangle")
     } else if (length1===length2 & length2!==length3 || length1!==length2 & length2==length3){
         alert("isoselice triangle")
-    } else if (length1!==length2 & length2!==length3 &length1!==length3){
+    } else if (length1!==length2 & length2!==length3 &length1!==length3 ){
         alert("scalene triangle")
-    } else if( totalLength==0 ){
-        alert("wabebebe mimi")
-
     }
 
 }
